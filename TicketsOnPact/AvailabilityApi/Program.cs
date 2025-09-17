2667fe7c27d79cc86d8edb40fbb7dd9191dd85ac
Łukasz Reszke <lukaszreszke93@gmail.com>
2025-09-17 21:31:24 +0200

    Not yet

--- TicketsOnPact/AvailabilityApi/Program.cs
+++ TicketsOnPact/AvailabilityApi/Program.cs
@@ -19,7 +19,6 @@ public partial class Program
         builder.Logging.SetMinimumLevel(LogLevel.Information);
         builder.Services.AddOpenApi();
         builder.Services.AddHealthChecks();
-   //     builder.Services.AddHostedService<HelloRabbitListener>();
 
         builder.Services.AddDbContextPool<AvailabilityContext>(opt =>
         {
@@ -63,54 +62,6 @@ public partial class Program
             }
         });
         
-        app.MapGet("/resource/create",
-            (AvailabilityContext context) =>
-            {
-                var resource = context.Resources.Add(new Resource() { Status = "Available" });
-                context.SaveChanges();
-                return Results.Ok(resource.Entity.Id);
-            });
-
-        app.MapGet("/resource/{id}", (int id, AvailabilityContext context) =>
-        {
-            var resource = context.Resources.FirstOrDefault(x => x.Id == id);
-            return resource != null ? Results.Ok(new { resource.Id, ResourceState = resource.Status, resource.Status, ValidUntilThisDate = DateTime.UtcNow }) : Results.NotFound();
-        });
-
-        app.MapGet("/block/{id}", (int id) =>
-        {
-            return id switch
-            {
-                1 => Results.Ok(new { Result = "Blocked" }),
-                2 => Results.BadRequest("Resource cannot be blocked"),
-                _ => Results.NotFound($"Resource with ID {id} not found")
-            };
-        });
-
-        app.MapGet("/resources", (AvailabilityContext context) =>
-        {
-            var resources = context.Resources.ToList();
-            return Results.Ok(resources);
-        });
-
-        app.MapPost("block/{id}", (int id, AvailabilityContext context) =>
-        {
-            context.Resources.FirstOrDefault(x => x.Id == id)!.Status = $"Blocked";
-            context.SaveChanges();
-        });
-        
-        app.MapPut("unblock/{id}", (int id, AvailabilityContext context) =>
-        {
-            context.Resources.FirstOrDefault(x => x.Id == id)!.Status = $"Unblocked";
-            context.SaveChanges();
-        });
-
-        app.MapGet("blocked", (AvailabilityContext context) =>
-        {
-            var resource = context.Resources.Where(x => x.Status == "Blocked").FirstOrDefault();
-            return Results.Ok(new { resource.Id, resource.Status });
-        });
-
         app.MapHealthChecks("/health");
  
         logger.LogInformation("Application configured and ready to start");
