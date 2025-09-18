f32dbfe0b21d67747a1c14fd412607332f0a5408
Łukasz Reszke <lukaszreszke93@gmail.com>
2025-09-18 20:31:26 +0200

    ProviderState and ProviderStateMiddleware

---
+++ TicketsOnPact/AvailabilityApiTests/ProviderState.cs
@@ -0,0 +1,9 @@
+namespace AvailabilityApiTests
+{
+    /// <summary>
+    /// Provider state DTO
+    /// </summary>
+    /// <param name="State">State description</param>
+    /// <param name="Params">State parameters</param>
+    public record ProviderState(string State, IDictionary<string, object> Params);
+}
